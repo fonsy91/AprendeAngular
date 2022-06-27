edaac099b0328d708a3e0fa15563ef9a0541abe6
alfonso91 <alfonso91fp@gmail.com>
2022-06-27 18:12:15 +0200

    Avances semana 13 al 17 Junio 2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   //En este apartado se escibre la logica del componente 
   //variable o propiedad 
@@ -14,10 +17,39 @@ export class AppComponent {
   numero1:number=0;
   numero2:number=0;
   resultado:number=0;
+  entradas:any[];
+
+  //Variable de tipo empleado situada en app>empleado.model.ts y agregamos unos cuantos empleados 
+  empleados:Empleado[]=[
+    new Empleado("Juan","Perez","Programador",1200),
+    new Empleado("Jaime","Daniel","Programador",1300),
+    new Empleado("Luis","Sanz","Programador",1400),
+    new Empleado("Laura","Briant","Programador",1500)
+  ];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
  
   //Constructor de la clase dentro de la logica del componente principal 
   constructor(){
     //console.log("Hola tio");
+
+    this.entradas=[
+      {titulo: "Python cada dia mas presente"},
+      {titulo: "Java presente desde hace mas de 20 años"},
+      {titulo: "javascript cada dia mas funcional"},
+      {titulo: "Donde quedo pascal"},
+    ];
+  }
+
+  //Funcion para agregar un empleado
+  agregarEmpleado(){
+    //con esto cogemos un empleado introducido por el formulario 
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    //Introducimos dicho empleado en el array 
+    this.empleados.push(miEmpleado);
   }
 
   //Funciones para la calculadora
@@ -28,5 +60,21 @@ export class AppComponent {
   restar():void{
     this.resultado=this.numero1-this.numero2;
   }
+
+  //DIRECTIVAS 1 DEL CURSO 
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  
+
+  registrarUsuario(){
+    //Cuando se pulsa el boton se cambia a true 
+    this.registrado=true;
+    this.mensaje="Usuario Registrado con Exito";
+  }
+
+  
+
 
 }
---
+++ src/app/empleado.model.ts
@@ -0,0 +1,15 @@
+//Clase empleado 
+export class Empleado{
+
+    constructor(nombre:string,apellido:string,cargo:string,salario:number){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.cargo=cargo;
+        this.salario=salario;
+    }
+
+    nombre:string="";
+    apellido:String="";
+    cargo:string="";
+    salario:number=0;
+}
